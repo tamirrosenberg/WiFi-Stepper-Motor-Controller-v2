6ba5cddaec307ef32982eea34515c8c503d7ccc7
Tamir Rosenberg <68561398+tamirrosenberg@users.noreply.github.com>
2022-12-24 02:05:14 -0800

    First version of the app

---
+++ N6JJ_WiFi_Stepper_Motor_Controller-v2.ino
@@ -0,0 +1,295 @@
+#include <ESP8266WiFi.h> //https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WiFi/src/ESP8266WiFi.h
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <DoubleResetDetect.h> https://github.com/jenscski/DoubleResetDetect
+#include <ESPAsyncTCP.h> //https://github.com/me-no-dev/ESPAsyncTCP
+#define WEBSERVER_H
+#include <ESPAsyncWebServer.h> //https://github.com/me-no-dev/ESPAsyncWebServer
+
+//Constants
+#define LED D4
+
+// maximum number of seconds between resets that
+// counts as a double reset 
+#define DRD_TIMEOUT 2.0
+
+// address to the block in the RTC user memory
+// change it if it collides with another usage of the address block
+#define DRD_ADDRESS 0x00
+
+DoubleResetDetect drd(DRD_TIMEOUT, DRD_ADDRESS);
+
+//Parameters
+String AP_ID = "reHamRadio-AP";
+String AP_PW = "rehamradio";
+
+//Variables
+const int enablePin = 0;                            // ESP8266 digital pin 0
+const int stepPin = 4;                              // ESP8266 digital pin 4
+const int dirPin = 5;                               // ESP8266 digital pin 5
+bool tuneStatus = false;
+String sliderValue = "2000";
+int sliderRange = 4000;
+const char* PARAM_INPUT = "value";
+
+// HTML web page
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML>
+<html>
+<head>
+<body style='background-color:#77a6f7; color:white;'>
+    <title>WiFi Stepper Motor Controller</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        body {
+            font-family: Arial;
+            text-align: center;
+            margin: 0px auto;
+            padding-top: 30px;
+        }
+        .button {
+            padding: 10px 20px;
+            padding-left: 10%;
+            padding-right: 10%;
+            font-size: 24px;
+            text-align: center;
+            outline: none;
+            color: #fff;
+            background-color: #2f4468;
+            border: none;
+            border-radius: 5px;
+            box-shadow: 0 6px #999;
+            cursor: pointer;
+            -webkit-touch-callout: none;
+            -webkit-user-select: none;
+            -khtml-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+        }
+        .button:hover {
+            background-color: #1f2e45
+        }
+        .button:active {
+            background-color: #1f2e45;
+            box-shadow: 0 4px #666;
+            transform: translateY(2px);
+        }
+        .slider {
+            -webkit-appearance: none;
+            margin: 0 auto;
+            border-radius: 25px;
+            width: 60%;
+            height: 25px;
+            background: #FFD65C;
+            outline: none;
+            -webkit-transition: .2s;
+            transition: opacity .2s;
+        }
+        .slider::-webkit-slider-thumb {
+            -webkit-appearance: none;
+            appearance: none;
+            width: 65px;
+            height: 35px;
+            border-radius: 25%;
+            background: #2f4468;
+            cursor: pointer;
+        }
+        .slider::-webkit-slider-thumb:hover {
+            background-color: #1f2e45
+        }
+        .slider::-moz-range-thumb {
+            width: 65px;
+            height: 35px;
+            border-radius: 25%;
+            background: #2f4468;
+            cursor: pointer;
+        }
+        .slider::-moz-range-thumb:hover {
+            background-color: #1f2e45
+        }
+    </style>
+    </head>
+    <body>
+        <h2><center>WiFi Stepper Motor Controller</center></h2>
+        <hr>
+        <p id="tuneStatus">Connecting...</p>
+        <p>slow<input type="range" direction="ltr" onchange="updateSpeedSlider(this)" id="speedSlider" min="0"
+                max="4000" value="" step="1" class="slider">fast</p>
+        <br>
+        <button class="button" onmousedown="toggleCheckbox('scanToggle');" ontouchstart="toggleCheckbox('scanToggle');">
+            SCAN </button>
+        <br><br><br>
+        <button class="button" onmousedown="toggleCheckbox('leftOn');" ontouchstart="toggleCheckbox('leftOn');"
+            onmouseup="toggleCheckbox('leftOff');" ontouchend="toggleCheckbox('leftOff');"><<<<</button>      
+        <button class="button" onmousedown="toggleCheckbox('rightOn');" ontouchstart="toggleCheckbox('rightOn');"
+            onmouseup="toggleCheckbox('rightOff');" ontouchend="toggleCheckbox('rightOff');">>>>></button>
+        <br><br><hr>
+        <font size="1"><center>Created by N6JJ - Community Version (v0.3-beta) 2022 </center></font>
+        <font size="1"><center><a href='https://www.youtube.com/c/reHamRadio' target='_blank'>re:HamRadio on YouTube</a></center></font>
+        <font size="1"><center><a href='https://rehamradio.com' target='_blank'>re:HamRadio Website</a></center></font>
+        <font size="1"><center><a href='https://github.com/tamirrosenberg/WiFi-Stepper-Motor-Controller-v2' target='_blank'>The project on GitHub</a></center></font>
+        <script>
+            function toggleCheckbox(x) {
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", "/" + x, true);
+                xhr.send();
+            }
+            function updateSpeedSlider(element) {
+                var sliderValue = document.getElementById("speedSlider").value;
+                //document.getElementById("textSliderValue").innerHTML = sliderValue;
+                console.log(sliderValue);
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", "/slider?value=" + sliderValue, true);
+                xhr.send();
+            }
+            function checkAndUpdateStatus() {
+                var xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById("tuneStatus").innerHTML =
+                            this.responseText;
+                    }
+                };
+                xhttp.open("GET", "/tuneStatus", true);
+                xhttp.send();
+            }
+            setInterval(checkAndUpdateStatus, 75);
+    
+        </script>
+    </body>
+</html>)rawliteral";
+
+AsyncWebServer server(80);
+
+void setup() {
+  //Init Serial USB
+  Serial.begin(115200);
+  Serial.println(F("Initialize System..."));
+
+  // WiFiManager
+  WiFiManager wm;
+   
+  bool res;
+  res = wm.autoConnect(AP_ID.c_str(),AP_PW.c_str()); // password protected access point
+  
+  if (drd.detect())
+     {
+         Serial.println("** Double reset boot **");
+         WiFi.disconnect();
+         wm.startConfigPortal(AP_ID.c_str(),AP_PW.c_str());
+         delay(1000);
+         ESP.restart();
+     }
+  
+  if(!res) {
+     Serial.println("Failed to connect");
+  } 
+  else {    
+     Serial.println("Connected!");
+  }
+  
+  // Declare pins as Outputs
+  pinMode(stepPin, OUTPUT);                           // step pin
+  pinMode(dirPin, OUTPUT);                            // direction pin
+  pinMode(enablePin, OUTPUT);                         // enable pin
+  pinMode(LED, OUTPUT);                               // onboard LED indicator
+  digitalWrite(enablePin, HIGH);                      // start with driver disable
+  
+  // Send web page to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html",index_html);
+  });
+
+  // Button Left On
+  server.on("/leftOn", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    Serial.println("Left On");
+    digitalWrite(dirPin, HIGH);                         // set direction to one side
+    tuneStatus = true;
+    request->send(200, "text/plain", "ok");
+  });
+
+  // Button Left Off
+  server.on("/leftOff", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    Serial.println("Left Off");
+    tuneStatus = false;
+    request->send(200, "text/plain", "ok");
+  });
+
+  // Button Right On
+  server.on("/rightOn", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    Serial.println("Right On");
+    digitalWrite(dirPin, LOW);                         // set direction to one side
+    tuneStatus = true;
+    request->send(200, "text/plain", "ok");
+  });
+
+  // Button Right Off
+  server.on("/rightOff", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    Serial.println("Right Off");
+    tuneStatus = false;
+    request->send(200, "text/plain", "ok");
+  });
+
+  // Button Scan Toggle
+  server.on("/scanToggle", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    Serial.println("Scan Toggle");
+    if (tuneStatus == false) {
+      tuneStatus = true;
+    }
+    else {
+      tuneStatus = false;
+    }
+    Serial.println({tuneStatus});
+    request->send(200, "text/plain", "ok");
+  });
+
+  server.on("/slider", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    if (request->hasParam(PARAM_INPUT)) {
+      inputMessage = request->getParam(PARAM_INPUT)->value();
+      sliderValue = inputMessage;
+    }
+    else {
+      inputMessage = "No message sent";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/tuneStatus", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    String statusValue = "Idle";
+    if(tuneStatus){
+      statusValue = "Tunning...";
+    }
+    request->send(200, "text/plain", statusValue);
+  });
+  
+  server.onNotFound(notFound);
+  server.begin();
+}
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Hmm... something is wrong... :(");
+}
+
+void loop() {
+  // -- Main Tuning Function --
+  if (tuneStatus) {
+    digitalWrite(LED, LOW);                           // turn LED indicator OFF
+    digitalWrite(enablePin, LOW);
+    Serial.println("slider: string-->" + sliderValue);
+    Serial.println("Tunning...");
+    digitalWrite(stepPin, HIGH);                           // perform a step
+    //digitalWrite(LED, LOW);                              // turn LED indicator OFF
+    delayMicroseconds(sliderRange-sliderValue.toInt());    // wait for the steps operation
+    digitalWrite(stepPin, LOW);                            // stop the step 
+    //digitalWrite(LED, HIGH);                             // turn LED indicator ON
+    delayMicroseconds(sliderRange-sliderValue.toInt());    // wait between steps
+  }
+  else {
+    digitalWrite(LED, HIGH);
+    digitalWrite(enablePin, HIGH);
+  }
+}
